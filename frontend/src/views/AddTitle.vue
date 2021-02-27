cdb3f7007d2108823e8ca2ff5ef7c6d6873de37a
Dale Terrell <tragicid@gmail.com>
2021-02-27 05:38:52 -0600

    vue

---
+++ frontend/src/views/AddTitle.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 class="title">Add Game</h1>
+
+    <div class="field">
+      <label class="label" for="gameTitle">Game Title</label>
+      <div class="controller">
+        <input id="gameTitle" class="input" type="text" />
+      </div>
+    </div>
+  </div>
+</template>
+
+
+

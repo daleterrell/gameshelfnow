13a6b2ff488f8db2d2f79561e9870fb3e9532b25
Dale Terrell <tragicid@gmail.com>
2021-02-27 10:50:00 -0600

    vue work

--- frontend/src/views/AddTitle.vue
+++ frontend/src/views/AddTitle.vue
@@ -5,7 +5,37 @@
     <div class="field">
       <label class="label" for="gameTitle">Game Title</label>
       <div class="controller">
-        <input id="gameTitle" class="input" type="text" />
+        <input
+          id="gameTitle"
+          class="input"
+          type="text"
+          v-model="game.gameTitle"
+          placeholder="Game Title"
+        />
+      </div>
+    </div>
+    <div class="field">
+      <label class="label" for="gameDesigner">Designer</label>
+      <div class="controller">
+        <input
+          id="Designer"
+          class="input"
+          type="text"
+          v-model="game.Designer"
+          placeholder="Designer"
+        />
+      </div>
+    </div>
+    <div class="field">
+      <label class="label" for="gamePublisher">Publisher</label>
+      <div class="controller">
+        <input
+          id="Publisher"
+          class="input"
+          type="text"
+          v-model="game.Publisher"
+          placeholder="Publisher"
+        />
       </div>
     </div>
   </div>
